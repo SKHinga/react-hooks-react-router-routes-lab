83386c2583fe567b2bb6709d79981a3e821866c3
Samuel Hinga <samuelhinga200@gmail.com>
2022-05-22 19:40:33 +0300

    Complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+const items = directors.map(direct=>(
+  <div key={direct.name}>
+    <h2>Name: {direct.name}</h2>
+    <p>Movies</p>
+    <ul>
+      {direct.movies.map(movie=><li key={movie}>{movie}</li>)}
+    </ul>
+  </div>
+))
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {items}
+  </div>);
 }
 
 export default Directors;
